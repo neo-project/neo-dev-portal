06c8a6b88d12b669dd7660e7bd121b67336f04f6
Apisit Toompkadee <apisit@olto.co>
2022-02-16 23:22:06 +0900

    update how to contribute

--- src/pages/contribute.js
+++ src/pages/contribute.js
@@ -5,16 +5,21 @@ export default function Contribute() {
     return (
         <Layout>
             <div className="twcontainer  mx-auto max-w-6xl px-4 lg:px-0 pt-20">
-                <div className="flex items-center mb-12">
-                    <svg width="23" height="14" viewBox="0 0 23 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M16 13L22 7L16 1" stroke="#10E6A0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                        <path d="M7 1L1 7L7 13" stroke="#10E6A0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    <span className="text-2xl font-semibold ml-3">How to Contribute</span>
+                
+                <div className="items-center mb-12 flex flex-col">
+                    <div className='bg-gray-100 rounded-full h-12 w-12 items-center justify-center flex mb-6'>
+                        <svg className='' width="23" height="14" viewBox="0 0 23 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M16 13L22 7L16 1" stroke="#10E6A0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M7 1L1 7L7 13" stroke="#10E6A0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </div>
+                    <h1 className="text-4xl text-secondary font-bold uppercase mb-4">HELP THE COMMUNITY</h1>
+                    <p className='text-secondary'>Neo documentation is a community-driven effort. Join us by contributing your knowledge to help Neo developers.</p>
                 </div>
                 <img src="/contribute/header.png" />
 
-                <div className="mt-12 flex flex-col gap-4">
+                <div className="mt-12 flex flex-col gap-4 mb-12">
+                    <h3>How to create tutorial</h3>
                     <p>1. Create a new directory in <code>tutorials</code> directory using this format   <code>YYYY-MM-DD-your-tutorial-title</code>
                         <p className="">YYYY-MM-DD is the date of your tutorial. Tutorials are sort by date in descending order.</p>
                     </p>
