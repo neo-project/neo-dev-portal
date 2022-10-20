80ce54fdba7bcd97adaf0f967bb3d162b119bf34
Apisit <apmotive@gmail.com>
2022-10-20 23:06:50 +0900

    base64 to utf8

--- src/pages/converter.js
+++ src/pages/converter.js
@@ -98,6 +98,16 @@ export default function Converter(props) {
                         ]
                     },
                     {
+                        title: "Base64 to UTF8 string",
+                        func: (t) => { return Neon.u.base642utf8(t) },
+                        codes: [
+                            {
+                                "label": "neon-js",
+                                "code": `Neon.u.base642utf8("${text}")`,
+                            }
+                        ]
+                    },
+                    {
                         title: "Base64 to Hex string",
                         func: (t) => { return Neon.u.base642hex(t) },
                         codes: [
@@ -151,7 +161,7 @@ export default function Converter(props) {
 
                             <div className={`mt-8 ${text === "" ? "hidden" : ""}`}>
                                 <p className='font-medium'>Conversion results</p>
-                                <div className='mt-4 flex flex-col gap-4'>
+                                <div className='mt-4 grid grid-cols-1 lg:grid-cols-2 gap-4'>
                                     {
                                         text && conversionList.map((c, i) => (
                                             <div key={`conversion-${i}`} className='border'>
@@ -189,14 +199,13 @@ export default function Converter(props) {
                                 <div className='mt-2'>
                                     {
                                         sampleTextsToConvert.map((t) => (
-                                            <div key={t} className='flex items-center text-secondary'>
+                                            <div key={t} className='flex items-center text-secondary break-all'>
                                                 {t}
                                                 <button onClick={(e) => { setText(t) }} className='ml-2 text-primary'>
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
                                                     </svg>
-
                                                 </button>
 
                                             </div>
